2978672b465efbcc0205abc6432fba290153d175
arjunQ21 <arjunq21@gmail.com>
2023-01-14 16:01:00 +0545

    Collision detection function added

--- script.js
+++ script.js
@@ -10,42 +10,53 @@ var context = canvas.getContext("2d");
 var fromTop = canvas.height / 2;
 var velocity = 0;
 var gravity = 0.1;
-var horizontalPipeSpacing = 150 ;
+var horizontalPipeSpacing = 200;
+var collided = false ;
 
 // make array of pipes
-var pipes = [] ;
-for(var i = 0 ; i < 5; i++){
-    pipes.push(getPipe());
+var pipes = [];
+for (var i = 0; i < 5; i++) {
+  pipes.push(getPipe());
 }
-console.log(pipes)
-
+console.log(pipes);
 
 setInterval(function () {
   context.clearRect(0, 0, canvas.width, canvas.height);
   velocity = velocity + gravity;
   fromTop = fromTop + velocity;
-  if (fromTop >= canvas.height - 80) {
-    fromTop = window.innerHeight - 80;
+  if (fromTop >= canvas.height - 40) {
+    fromTop = window.innerHeight - 40;
   }
   console.log(fromTop);
 
   // drwaing pipes
 
-  for(var i = 0 ; i < pipes.length; i++){
-        var pipe = pipes[i] ;
-        context.fillStyle = "green";
-        context.fillRect(pipe.top.x, pipe.top.y, pipe.top.width, pipe.top.height);
-        context.fillRect(
-          pipe.bottom.x,
-          pipe.bottom.y,
-          pipe.bottom.width,
-          pipe.bottom.height,
-        );
+  for (var i = 0; i < pipes.length; i++) {
+    var pipe = pipes[i];
+    context.fillStyle = "green";
+    context.fillRect(pipe.top.x, pipe.top.y, pipe.top.width, pipe.top.height);
+    context.fillRect(
+      pipe.bottom.x,
+      pipe.bottom.y,
+      pipe.bottom.width,
+      pipe.bottom.height,
+    );
 
+    // moving pipes
+    pipe.top.x = pipe.top.x - 1;
+    pipe.bottom.x = pipe.bottom.x - 1;
+
+    // removing hidden pipes from our array
+    if (pipe.top.x < 0) {
+      pipes.splice(i, 1);
+      pipes.push(getPipe());
+    }
+
+    console.log(pipes);
   }
 
   context.fillStyle = "red";
-  context.fillRect(20, fromTop, 80, 80);
+  context.fillRect(200, fromTop, 40, 40);
 }, 10);
 
 window.addEventListener("keydown", function () {
@@ -53,11 +64,15 @@ window.addEventListener("keydown", function () {
   velocity = -8;
 });
 
-
-
 function getPipe() {
+  horizontalPipeSpacing = horizontalPipeSpacing + 270;
 
-    horizontalPipeSpacing = horizontalPipeSpacing + horizontalPipeSpacing ;
+
+  if(pipes.length > 0){
+      var xOfLastPipeInList = pipes[pipes.length - 1].top.x ;
+      horizontalPipeSpacing = xOfLastPipeInList + 270;
+  }
+
   function randomIntFromInterval(min, max) {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -65,7 +80,7 @@ function getPipe() {
 
   var topPipeHeight = randomIntFromInterval(0, canvas.height / 2);
 
-  const verticalGap = 200;
+  const verticalGap = 300;
   return {
     top: {
       x: horizontalPipeSpacing,
@@ -80,4 +95,16 @@ function getPipe() {
       height: canvas.height - topPipeHeight - verticalGap,
     },
   };
+}
+
+
+function checkCollision(rect1, rect2){
+    collided = false ;
+    if (rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.height + rect1.y > rect2.y) {
+        // collision detected!
+collided = true ;
+     }
 }
